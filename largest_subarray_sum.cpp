00f0ac8eb6af159028cf1efc82f95edcafbd4369
sardanna <35367634+sardanna@users.noreply.github.com>
2018-04-02 10:26:48 +0530

    Update largest_subarray_sum.cpp

--- largest_subarray_sum.cpp
+++ largest_subarray_sum.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-int sum(int a[],int n,int *start,int *end){
-  int max_so_far = INT_MIN,max_ending_here = 0,tmp = 0;
+int sum(int a[],int n,int *start,int *end){                               //function call
+  int max_so_far = INT_MIN,max_ending_here = 0,tmp = 0;                   //tmp will take care of changing start index
   for(int i = 0;i<n;i++){
     max_ending_here = max_ending_here + a[i];
-    if(max_ending_here < 0){
+    if(max_ending_here < 0){                                              //change the maximum ending here
       max_ending_here = 0;
       tmp = i+1;
     }
-    else if(max_so_far < max_ending_here){
+    else if(max_so_far < max_ending_here){                                //change maximum so far
       max_so_far = max_ending_here;
       *start = tmp;
       *end = i;
